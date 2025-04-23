b6c88561483820bd5bc0c10a592d7db8b5691a5b
Steve Yonkeu <yokwejuste@yahoo.com>
2025-04-23 04:52:58 +0100

    Remove trailing space in filename

--- lavoixlocale/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls.conf import include, re_path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^accounts/', include('allauth.urls')),
-]
